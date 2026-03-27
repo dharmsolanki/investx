52a8404a0ede42ebac48c6c8dff2453c278bb604
Dharm Solanki <dharmsolanki1010@gmail.com>
2026-03-27 17:55:54 +0530

    fix symbolic link

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Storage symlink auto-create
+        $link   = public_path('storage');
+        $target = storage_path('app/public');
+
+        if (! file_exists($link) && file_exists($target)) {
+            symlink($target, $link);
+        }
     }
 }
