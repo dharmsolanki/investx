9ba43355f190276932bd9e923ced005238b344fb
Dharm Solanki <dharmsolanki1010@gmail.com>
2026-05-11 10:32:46 +0530

    import InvestmentController

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\InvestmentPlan;
 use App\Models\Transaction;
+use App\Http\Controllers\InvestmentController;  // ← ADD
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
